32062cf27bce9c674613c22662c79b3193068d25
sergiotaerri <sergio2000jr@gmail.com>
2020-10-30 02:18:16 -0300

    cake-acl up to 11.2.4.1

---
+++ cake-acl/app/models/widget.php
@@ -0,0 +1,16 @@
+<?php
+class Widget extends AppModel {
+	var $name = 'Widget';
+	var $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => true, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
